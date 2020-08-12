0dbb4bd157857cb05961d3ffb9717519fc74c8c9
Mikael Toernwall <mikael.tornwall@aalto.fi>
2020-08-12 15:38:26 +0200

    update process.env.PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app       = express()
 
 app.use(express.static(__dirname + '/public'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
